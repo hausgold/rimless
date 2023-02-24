3281bbdcc7e0807f751b699e0516c20c57d56a33
deployhausgold <deploy@hausgold.de>
2023-02-24 11:15:32 +0000

    [Release] Bumped the version to 1.4.0.

--- lib/rimless/version.rb
+++ lib/rimless/version.rb
@@ -3,7 +3,7 @@
 # The gem version details.
 module Rimless
   # The version of the +rimless+ gem
-  VERSION = '1.3.0'
+  VERSION = '1.4.0'
 
   class << self
     # Returns the version of gem as a string.
