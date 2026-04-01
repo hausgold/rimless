fc53965ece03d6165cb589db439540e62d3deb6b
Hermann Mayer <hermann.mayer92@gmail.com>
2026-04-01 11:23:40 +0200

    Updated the kafka-playground.
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

--- doc/kafka-playground/config/environment.rb
+++ doc/kafka-playground/config/environment.rb
@@ -5,53 +5,21 @@ require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'active_support'
 require 'active_support/all'
 require 'json'
-require 'pp'
 
 Bundler.require(:default)
 ActiveSupport.eager_load!
 
-AppLogger = Logger.new(STDOUT)
+AppLogger = Logger.new($stdout)
 AppLogger.level = Logger::FATAL
-AppLogger.level = Logger::DEBUG if ENV.fetch('DEBUG', '').match? /true|1|on/
+AppLogger.level = Logger::DEBUG if ENV.fetch('DEBUG', '').match?(/true|1|on/)
 
-Rimless.configure do |conf|
-  conf.env = 'production'
-  conf.app_name = 'playground_app'
-  conf.client_id = 'playground'
-  conf.logger = AppLogger
-  conf.kafka_brokers = ['kafka://kafka.playground.local:9092']
-  conf.schema_registry_url = 'http://schema-registry.playground.local'
-end
-
-KafkaClient = Kafka.new(Rimless.configuration.kafka_brokers, logger: AppLogger)
-
-# +Resolv+ is a thread-aware DNS resolver library written in Ruby. Some newer
-# networking libraries like excon (>=0.85.0) makes use of it instead of the
-# regular glibc facility. This raises an issue for our local development as we
-# use the mDNS stack which is configured in every Docker image accordingly
-# (Avahi, libnss[-mdns]). The default resolver of +Resolv+ does not include the
-# mDNS stack so we have to reconfigure it here for local usage only.
-#
-# See: https://docs.ruby-lang.org/en/2.7.0/Resolv.html
-require 'resolv'
-Resolv::DefaultResolver.replace_resolvers(
-  [
-    Resolv::Hosts.new,
-    Resolv::MDNS.new,
-    Resolv::DNS.new
-  ]
-)
-
-def topic?(name)
-  @topic_conf = KafkaClient.describe_topic(name)
-rescue Kafka::UnknownTopicOrPartition
-  false
-end
+Dir[File.expand_path('initializers/*.rb', __dir__)].each { |f| require f }
 
 def args!
   app = Thor.descendants.map(&:to_s)
-                        .reject { |klass| klass.include? '::' }.first
-  raise "No Thor application class was found." unless app
+            .reject { |klass| klass.include? '::' }.first
+  raise 'No Thor application class was found.' unless app
+
   app = app.constantize
 
   help = ARGV.any? { |arg| %w[help -h --help].include?(arg) }
